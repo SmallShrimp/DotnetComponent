44bc06f832857692f3a8758cff0729d04e331f60
gexiaoxu <gexiaoxu@126.com>
2017-11-23 11:07:28 +0800

    加密解密方法

--- src/Xugege.DotnetExtension.Com/Helper/StringHelper.cs
+++ src/Xugege.DotnetExtension.Com/Helper/StringHelper.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Xugege.DotnetExtension.Com.Helper
 {
     public class StringHelper
     {
+
+        private const string EncryptKey = "qweasdzx";
+        private const string EncryptIV = "rtyuiogj";
+
         public static string CreateIdByTime()
         {
             TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
@@ -37,5 +44,75 @@ namespace Xugege.DotnetExtension.Com.Helper
             byte[] buffer = Guid.NewGuid().ToByteArray();
             return BitConverter.ToInt64(buffer, 0);
         }
+
+        #region 加密解密
+
+        public static string Encrypt(string sourceString, string key = EncryptKey, string iv = EncryptIV)
+        {
+            try
+            {
+                byte[] btKey = Encoding.UTF8.GetBytes(key);
+
+                byte[] btIV = Encoding.UTF8.GetBytes(iv);
+
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] inData = Encoding.UTF8.GetBytes(sourceString);
+                    try
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(btKey, btIV),
+                            CryptoStreamMode.Write))
+                        {
+                            cs.Write(inData, 0, inData.Length);
+
+                            cs.FlushFinalBlock();
+                        }
+
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                    catch
+                    {
+                        return sourceString;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static string Decrypt(string encryptedString, string key = EncryptKey, string iv = EncryptIV)
+        {
+            byte[] btKey = Encoding.UTF8.GetBytes(key);
+
+            byte[] btIV = Encoding.UTF8.GetBytes(iv);
+
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] inData = Convert.FromBase64String(encryptedString);
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(btKey, btIV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inData, 0, inData.Length);
+
+                        cs.FlushFinalBlock();
+                    }
+
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+
+        #endregion
     }
 }
